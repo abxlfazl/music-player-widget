523431bb9ce3123432da483c364edb96f04227ee
abxlfazl <abolfazlkhorshidi99@gmail.com>
2022-09-14 00:27:37 +0430

    Add the data

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 /** @jsx dom */
 
-function App() {
+function App({ songs }) {
   return (
     <div class="music-player flex-column">
       <Slider />
@@ -9,7 +9,42 @@ function App() {
   );
 }
 
-document.getElementById("root").appendChild(<App />);
+fetch("../data.json")
+  .then((respone) => respone)
+  .then((data) => data.json())
+  .then((result) => {
+    const songs = result.songs;
+
+    function downloadTheFiles(media, input) {
+      return Promise.all(
+        input.map((song) => {
+          return new Promise((resolve) => {
+            const url = song.files[media];
+            const req = new XMLHttpRequest();
+            req.open("GET", url, true);
+            req.responseType = "blob";
+            req.send();
+            req.onreadystatechange = () => {
+              if (req.readyState === 4) {
+                if (req.status === 200) {
+                  const blob = req.response;
+                  const file = URL.createObjectURL(blob);
+                  song.files[media] = file;
+                  resolve(song);
+                }
+              }
+            };
+          });
+        })
+      );
+    }
+
+    downloadTheFiles("cover", songs).then((respone) => {
+      downloadTheFiles("song", respone).then((data) => {
+        document.getElementById("root").appendChild(<App songs={data} />);
+      });
+    });
+  });
 
 function handleResize() {
   const vH = window.innerHeight * 0.01;
