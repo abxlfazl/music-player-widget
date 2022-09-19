82f67fd1723f1b8688f0048597c33396723c2e8d
abxlfazl <abolfazlkhorshidi99@gmail.com>
2022-09-19 14:53:14 +0430

    Add animation to the slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,42 @@
 /** @jsx dom */
 
+let isLocked = false;
+let songName_elmnt = null;
+let sliderImgs_elmnt = null;
+let singerName_elmnt = null;
+let musicPlayerInfo_elmnt = null;
+
 function App({ songs }) {
+  let indexSong = 0;
+  const songsLength = songs.length - 1;
+
+  function handleChangeMusic({ isPrev = false }) {
+    if (isLocked) return;
+
+    indexSong = isPrev ? (indexSong -= 1) : (indexSong += 1);
+
+    if (indexSong < 0) {
+      indexSong = 0;
+      return;
+    }
+
+    if (indexSong > songsLength) {
+      indexSong = songsLength;
+      return;
+    }
+
+    updateInfo(singerName_elmnt, songs[indexSong].artist);
+    updateInfo(songName_elmnt, songs[indexSong].songName);
+
+    setProperty(sliderImgs_elmnt, "--index", -indexSong);
+    setBodyBg(songs[indexSong].bg);
+  }
+
+  setBodyBg(songs[0].bg);
+
   return (
     <div class="music-player flex-column">
-      <Slider slides={songs} />
+      <Slider slides={songs} handleChangeMusic={handleChangeMusic} />
       <Playlist list={songs} />
     </div>
   );
@@ -41,16 +74,78 @@ fetch("../data.json")
 
     downloadTheFiles("cover", songs).then((respone) => {
       downloadTheFiles("song", respone).then((data) => {
-        document.getElementById("root").appendChild(<App songs={data} />);
+        querySelector("#root").appendChild(<App songs={data} />);
+
+        sliderImgs_elmnt = querySelector(".slider__imgs");
+        songName_elmnt = querySelector(".music-player__song-name");
+        musicPlayerInfo_elmnt = querySelector(".music-player__info");
+        singerName_elmnt = querySelector(".music-player__singer-name");
+
+        controlSubtitleAnimation(musicPlayerInfo_elmnt, songName_elmnt);
+        controlSubtitleAnimation(musicPlayerInfo_elmnt, singerName_elmnt);
       });
     });
   });
+
+function controlSubtitleAnimation(parent, child) {
+  const element = child.firstChild;
+
+  if (child.clientWidth > parent.clientWidth) {
+    if (child.classList.contains("isSubtitle")) return;
+    child.appendChild(element.cloneNode(true));
+    child.classList.add("isSubtitle");
+  } else {
+    child.classList.remove("isSubtitle");
+  }
+
+  setProperty(child.parentElement, "width", `${element.clientWidth}px`);
+}
 
 function handleResize() {
   const vH = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vH", `${vH}px`);
 }
 
+function querySelector(target) {
+  return document.querySelector(target);
+}
+
+function setProperty(target, prop, value = "") {
+  target.style.setProperty(prop, value);
+}
+
+function setBodyBg(color) {
+  setProperty(document.body, "--body-bg", color);
+}
+
+function updateInfo(target, value) {
+  while (target.firstChild) {
+    target.removeChild(target.firstChild);
+  }
+
+  const targetChild_elmnt = document.createElement("div");
+  targetChild_elmnt.appendChild(document.createTextNode(value));
+  target.appendChild(targetChild_elmnt);
+  controlSubtitleAnimation(musicPlayerInfo_elmnt, target);
+}
+
 handleResize();
+
 window.addEventListener("resize", handleResize);
 window.addEventListener("orientationchange", handleResize);
+window.addEventListener("transitionstart", ({ target }) => {
+  if (target === sliderImgs_elmnt) {
+    isLocked = true;
+    setProperty(sliderImgs_elmnt, "will-change", "transform");
+  }
+});
+window.addEventListener("transitionend", ({ target, propertyName }) => {
+  if (target === sliderImgs_elmnt) {
+    isLocked = false;
+    setProperty(sliderImgs_elmnt, "will-change", "auto");
+  }
+  if (target.classList.contains("slider") && propertyName === "height") {
+    controlSubtitleAnimation(musicPlayerInfo_elmnt, songName_elmnt);
+    controlSubtitleAnimation(musicPlayerInfo_elmnt, singerName_elmnt);
+  }
+});
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,26 @@
 /** @jsx dom */
 
-function Slider({ slides }) {
+function Slider({ slides, handleChangeMusic }) {
+  function handleResizeSlider({ target }) {
+    if (isLocked) {
+      return;
+    } else if (target.classList.contains("music-player__info")) {
+      this.classList.add("resize");
+      setProperty(this, "--controls-animate", "down running");
+      return;
+    } else if (target.classList.contains("music-player__playlist-button")) {
+      this.classList.remove("resize");
+      setProperty(this, "--controls-animate", "up running");
+      return;
+    }
+  }
+
   return (
-    <div class="slider center">
+    <div class="slider center" onClick={handleResizeSlider}>
       <div class="slider__content center">
+        <button class="music-player__playlist-button center button">
+          <i class="icon-playlist" />
+        </button>
         <button class="music-player__broadcast-guarantor center button">
           <i class="icon-play" />
           <i class="icon-pause" />
@@ -15,14 +32,28 @@ function Slider({ slides }) {
         </div>
       </div>
       <div class="slider__controls center">
-        <button class="slider__switch-button flex-row button">
+        <button
+          class="slider__switch-button flex-row button"
+          onClick={() => handleChangeMusic({ isPrev: true })}
+        >
           <i class="icon-back" />
         </button>
         <div class="music-player__info text_trsf-cap">
-          <div class="music-player__singer-name">{slides[0].artist}</div>
-          <div class="music-player__song-name">{slides[0].songName}</div>
+          <div>
+            <div class="music-player__singer-name">
+              <div>{slides[0].artist}</div>
+            </div>
+          </div>
+          <div>
+            <div class="music-player__song-name">
+              <div>{slides[0].songName}</div>
+            </div>
+          </div>
         </div>
-        <button class="slider__switch-button flex-row button">
+        <button
+          class="slider__switch-button flex-row button"
+          onClick={() => handleChangeMusic({ isPrev: false })}
+        >
           <i class="icon-next" />
         </button>
         <div class="progress center">
